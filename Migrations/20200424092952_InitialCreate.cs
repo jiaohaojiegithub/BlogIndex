5144239d9ad171a0b713e00185fc0fa2fbe2aa3d
jiaohaojiegithub <15949529192@163.com>
2020-04-24 17:57:44 +0800

    添加项目文件。

---
+++ Migrations/20200424092952_InitialCreate.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogIndex.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BaseAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedTime = table.Column<DateTime>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    AttributeName = table.Column<string>(nullable: true),
+                    AttributeValue = table.Column<string>(nullable: true),
+                    AttributeType = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    GetUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseAttributes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    ModifiedTime = table.Column<DateTime>(nullable: true),
+                    Url = table.Column<string>(type: "varchar(200)", nullable: false),
+                    FamilyName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true, computedColumnSql: "[FamilyName]+[LastName]"),
+                    Timestamp = table.Column<byte[]>(rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MxQuestionCategories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedTime = table.Column<DateTime>(nullable: true),
+                    CategoryName = table.Column<string>(nullable: true),
+                    ParentId = table.Column<long>(nullable: false),
+                    ParentName = table.Column<string>(nullable: true),
+                    Hierarchy = table.Column<int>(nullable: false),
+                    Sort = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MxQuestionCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MxQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedTime = table.Column<DateTime>(nullable: true),
+                    Question = table.Column<string>(nullable: true),
+                    QuestionType = table.Column<string>(nullable: false),
+                    QuestionCate = table.Column<string>(nullable: true),
+                    Answer = table.Column<string>(nullable: true),
+                    Options = table.Column<string>(nullable: true),
+                    Tags = table.Column<string>(nullable: true),
+                    Mx_QuestionCategoryId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MxQuestions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MxQuestions_MxQuestionCategories_Mx_QuestionCategoryId",
+                        column: x => x.Mx_QuestionCategoryId,
+                        principalTable: "MxQuestionCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MxQuestions_Mx_QuestionCategoryId",
+                table: "MxQuestions",
+                column: "Mx_QuestionCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaseAttributes");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+
+            migrationBuilder.DropTable(
+                name: "MxQuestions");
+
+            migrationBuilder.DropTable(
+                name: "MxQuestionCategories");
+        }
+    }
+}
---
+++ Models/BloggingContext.cs
@@ -0,0 +1,75 @@
+﻿using BlogIndex.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogIndex.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory
+                                 = LoggerFactory.Create(builder =>
+                                 {
+                                     builder
+                                        .AddFilter((category, level) =>
+                                            category == DbLoggerCategory.Database.Command.Name
+                                            && level == LogLevel.Information)
+                                        .AddConsole();
+                                 });
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+        : base(options)
+        { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Base_Attribute> BaseAttributes { get; set; }
+        public DbSet<Mx_Question> MxQuestions { get; set; }
+        public DbSet<Mx_QuestionCategory> MxQuestionCategories { get; set; }
+        public DbSet<Mx_Attribute> MxAttributes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory) // Warning: Do not create a new ILoggerFactory instance each time
+                .UseSqlServer(
+                    //@"Server=(localdb)\\mssqllocaldb;Database=Blogging;Trusted_Connection=True;"
+                    @"Data Source=.;Initial Catalog=Blogging;Integrated Security=True"
+                    , options => options.EnableRetryOnFailure());
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>(e =>
+            {
+                e.HasKey(b => b.Id);
+                e.Property(b => b.Id).ValueGeneratedOnAdd();
+                e.Property(b => b.Url).IsRequired();
+                e.Property(b => b.IsDeleted).HasDefaultValue<bool>(false);//设置默认值
+                e.Property(b => b.Name).HasComputedColumnSql("[FamilyName]+[LastName]");//关系型数据库计算其值
+                e.Property(b => b.LastName).IsConcurrencyToken();//并发标记控制
+                e.Property(b => b.Timestamp).IsRowVersion();//在每次插入或更新行时，数据库会自动为其生成新值.确认数据操作的并发唯一性
+                //e.HasData(new Blog { FamilyName = "焦", LastName = "豪杰", Url = "http://baidu.com" });//数据种子
+            });
+            modelBuilder.Entity<Base_Attribute>(e =>
+            {
+                e.HasKey(b => b.Id);
+                e.Property(b => b.Id).ValueGeneratedOnAdd();
+                e.HasDiscriminator<string>("Discriminator")//鉴别器 HasDiscriminator(b=>b.Discriminator)
+                 .HasValue<Base_Attribute>("Base")
+                 .HasValue<Mx_Attribute>("Mx");
+                //e.Property(b => b.AttributeName).HasColumnName("AttributeName");//共享列 列名相同则共享列
+            });
+            modelBuilder.Entity<Mx_Attribute>(e =>
+            {
+                //e.Property(b => b.AttributeName).HasColumnName("AttributeName");//共享列 列名相同则共享列
+            });
+            modelBuilder.Entity<Mx_Question>(e =>
+            {
+                e.HasOne(b => b.Mx_QuestionCategory).WithMany(p => p.Mx_Questions)//一对一 对应单条类别  反向导航 类别对应多条题目
+                 .HasForeignKey(b=>b.Mx_QuestionCategoryId)//外键设置
+                 //.IsRequired() //必选关系
+                 .OnDelete(DeleteBehavior.ClientSetNull)//级联删除，对于可选关系设置 导航属性若删除 则外键设置为null
+                 ;
+                e.Property(b => b.QuestionType).HasConversion<string>();//内置枚举值转换
+            });
+           
+        }
+    }
+}
